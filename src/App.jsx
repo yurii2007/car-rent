fc4fefb7dde563815cd36b715dd93909812d3339
yurii2007 <yurash862@gmail.com>
2023-10-06 09:57:10 +0300

    added routing to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import { Layout } from "./components/Layout";
+import { Home } from "./pages/Home";
+import { Catalog } from "./pages/Catalog";
+import { Favorites } from "./pages/Favorites";
+
 export const App = () => {
-  return <div>deploy check</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Route>
+      <Route path="*" element={<Navigate to="/" replace={true} />} />
+    </Routes>
+  );
 };
 
 export default App;
