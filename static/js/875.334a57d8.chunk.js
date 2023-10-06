cf2cff6facb88d9ec10878df508f706df1a1ff31
yurii2007 <135147654+yurii2007@users.noreply.github.com>
2023-10-06 18:24:50 +0000

    Deploying to gh-pages from @ yurii2007/car-rent@5011dbd2593050975c846fd7ceef581152034ab6 🚀

---
+++ static/js/875.334a57d8.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcar_rent=self.webpackChunkcar_rent||[]).push([[875],{1875:function(e,n,r){r.r(n);var t=r(9408),c=r(184);n.default=function(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsx)(t.H,{})})}}}]);
+//# sourceMappingURL=875.334a57d8.chunk.js.map
