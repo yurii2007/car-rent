815044d0a60de5635348cd835b819f7e1ac7eca5
yurii2007 <yurash862@gmail.com>
2023-10-06 11:12:01 +0300

    added script for routing in index html

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from "react-router-dom";
 
 export const Layout = () => {
   return (
-    <div>
+    <>
       <Suspense fallback={<div>Loading...</div>}>
         <Outlet />
       </Suspense>
-    </div>
+    </>
   );
 };
