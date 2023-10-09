a00dfbe6f2ba281044ab33337958483ca3c850e9
yurii2007 <yurash862@gmail.com>
2023-10-09 15:28:54 +0300

    header styles
    added react responsive
    added styles to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,9 @@
+import { useRef, useState } from "react";
 import { ReactComponent as Logo } from "../../images/svg/logo.svg";
 import burgerMenu from "../../images/animation/burgerAnim.json";
 import Lottie from "lottie-react";
 import { Navigation } from "../Navigation/Navigation";
-import { useRef, useState } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const style = {
   width: 32,
@@ -12,8 +13,10 @@ const style = {
 export const Header = () => {
   const lottieRef = useRef();
   const [isOpenNav, setOpenNav] = useState(false);
+  const isMobile = useMediaQuery({ query: "(max-width: 767.9px)" });
 
   const onBurgerClick = () => {
+    if (!isMobile) return;
     setOpenNav(!isOpenNav);
     isOpenNav
       ? lottieRef.current.setDirection(-1)
@@ -26,8 +29,8 @@ export const Header = () => {
 
   return (
     <header>
-      <div className="flex justify-between items-center py-[28px] px-[16px] max-w-[375px] md:max-w-[1024px] xl:max-w-[1440px] my-0 mx-auto">
-        <Logo width={40} height={40} />
+      <div className="flex justify-between items-center py-[28px] px-[16px] xl:px-[24px] max-w-[375px] md:max-w-[1024px] xl:max-w-[1440px] my-0 mx-auto">
+        <Logo width={50} height={50} />
         <Lottie
           onClick={onBurgerClick}
           lottieRef={lottieRef}
@@ -37,7 +40,7 @@ export const Header = () => {
           style={style}
           className="md:hidden"
         />
-        <Navigation />
+        <Navigation setOpenNav={onBurgerClick} isOpenNav={isOpenNav} />
       </div>
     </header>
   );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,11 +1,29 @@
+import { useEffect } from "react";
 import { NavLink } from "react-router-dom";
 
-export const Navigation = () => {
+export const Navigation = ({ isOpenNav, setOpenNav }) => {
+  useEffect(() => {
+    document.body.style.overflow = "visible";
+    if (isOpenNav) document.body.style.overflow = "hidden";
+  }, [isOpenNav]);
+
   return (
-    <nav className="hidden md:flex">
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/catalog">Catalog</NavLink>
-      <NavLink to="/favorites">Favorites</NavLink>
+    <nav
+      className={`absolute -left-[100%] flex ease-in-out duration-200 [&>.active]:underline ${
+        isOpenNav
+          ? "left-[0] top-0 z-10 flex-col gap-[24px] pt-[40px] pl-[20px] w-[70vw] bg-[#F9F9F9] h-screen"
+          : ""
+      } md:static gap-[18px]`}
+    >
+      <NavLink onClick={setOpenNav} className="text-[20px] font-medium leading-[20px]" to="/">
+        Home
+      </NavLink>
+      <NavLink onClick={setOpenNav} className="text-[20px] font-medium leading-[20px]" to="/catalog">
+        Catalog
+      </NavLink>
+      <NavLink onClick={setOpenNav} className="text-[20px] font-medium leading-[20px]" to="/favorites">
+        Favorites
+      </NavLink>
     </nav>
   );
 };
